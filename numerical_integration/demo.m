5fe83c2120ac489b523aaeff33bdd348f78751d3
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2019-03-17 14:07:15 +0600

    added simpson one-third method

--- numerical_integration/demo.m
+++ numerical_integration/demo.m
@@ -6,7 +6,7 @@ clear;
 % N = No. of intervals
 % a = lower limit
 % b = upper limit
-N = 10;
+N = 50;
 a = 0;
 b = 10;
 
@@ -17,24 +17,36 @@ Function = @ (x) (2 .* sin(x) .^ 2 + 5 .* cos(x + 3));
 rect_results = zeros(1, N);
 midpoint_results = zeros(1, N);
 trapezoidal_results = zeros(1, N);
+simpson_one_third_results = zeros(1, N);
 
 %% for number of intervals  from 1 till N, calculate the numerically intgrated value
 for i = 1:N
    rect_results(i) = rectangle_method(Function, a, b, i);
    midpoint_results(i) = midpoint_method(Function, a, b, i);
    trapezoidal_results(i) = trapezoidal_method(Function, a, b, i);
+   simpson_one_third_results(i) = simpson_one_third_method(Function, a, b, i);
 end
 
 %% calculate the actual value (Ground Truth)
 actual_val = integral(Function, a, b) * ones(1, N);
 
-%% do the plotting with the results
+%% do plotting with the results
+% convert function handle to string
+function_string = strrep(char(Function),'@(x)','');
+function_string = strrep(function_string, '.', '');
+title_string = sprintf("A comparison among the numerical integration methods for the function \nf(x) = %s", function_string);
+
+% plot the values
 plot(rect_results, '-or'); hold on;
 plot(midpoint_results, '-+g'); hold on;
 plot(trapezoidal_results, '-.b'); hold on;
+plot(simpson_one_third_results, '-sm'); hold on;
 plot(actual_val, '-*c'); hold on;
-legend("Rectangle Method", "Mid-point Method", "Trapezoidal Method", "Actual Value");
-title("A comparison among the numerical integration methods", 'fontsize', 16);
+
+legend("Rectangle Method", "Mid-point Method", "Trapezoidal Method", "Simpson's One-Third Method", "Actual Value");
+
+title(title_string, 'fontsize', 16);
 xlabel('Number of Intervals','fontsize',14);
 ylabel ('Function Value','fontsize',14);
+
 grid on;
---
+++ numerical_integration/simpson_one_third_method.m
@@ -0,0 +1,19 @@
+function I = simpson_one_third_method(Function, lower_interval, upper_interval, no_of_intervals)
+    % the number of subintervals must be even, or the no_of_intervals must
+    % be odd in order for simpson's one third rule to work
+    if mod(no_of_intervals, 2) ~= 0
+        no_of_intervals = no_of_intervals + 1;
+    end
+    
+    % calculate the step-size h
+    h = (upper_interval - lower_interval) / no_of_intervals;
+    % calculate the x values for the stepsize
+    x_vector = lower_interval:h:upper_interval;
+    % sum up the even indexed x values
+    sum_even = sum(Function(x_vector(2:2:no_of_intervals)));
+    % sum up the odd indexed x values
+    sum_odd = sum(Function(x_vector(3:2:no_of_intervals-1)));
+
+    % finc the numerical value of integration
+    I = (h/3) * (Function(lower_interval) + 4 * sum_even + 2 * sum_odd + Function(upper_interval));
+end
